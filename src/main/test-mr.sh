7a9608638170da518b17bd9d98cf8f227ef3af4f
oldmomsimith <oldmomsimith@protonmail.com>
2024-11-14 23:06:03 +0800

    完成所有内容,修复test-mr

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -7,18 +7,17 @@
 # un-comment this to run the tests with the Go race detector.
 # RACE=-race
 
-#if [[ "$OSTYPE" = "darwin"* ]]
-#then
-#  if go version | grep 'go1.17.[012345]'
-#  then
-#    # -race with plug-ins on x86 MacOS 12 with
-#    # go1.17 before 1.17.6 sometimes crash.
-#    RACE=
-#    echo '*** Turning off -race since it may not work on a Mac'
-#    echo '    with ' `go version`
-#  fi
-#fi
-RACE=-race
+if [[ "$OSTYPE" = "darwin"* ]]
+then
+  if go version | grep 'go1.17.[012345]'
+  then
+    # -race with plug-ins on x86 MacOS 12 with
+    # go1.17 before 1.17.6 sometimes crash.
+    RACE=
+    echo '*** Turning off -race since it may not work on a Mac'
+    echo '    with ' `go version`
+  fi
+fi
 
 ISQUIET=$1
 maybe_quiet() {
